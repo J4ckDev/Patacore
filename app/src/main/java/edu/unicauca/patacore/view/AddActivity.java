4bfdb53ce5218bb923220fd388b238ec8bfc870f
J4ckDev <j4ckdev@gmail.com>
2019-12-11 21:03:20 -0500

    llenar productos funciona

--- app/src/main/java/edu/unicauca/patacore/view/AddActivity.java
+++ app/src/main/java/edu/unicauca/patacore/view/AddActivity.java
@@ -58,7 +58,7 @@ public class AddActivity extends AppCompatActivity {
 
 
         gestorSQL = new GestorSQL(context);
-        //llenarProductos ();
+        llenarProductos ();
         //consultarListaProductos();
         //regPedido();
         //actualizarEstadoSelected();
